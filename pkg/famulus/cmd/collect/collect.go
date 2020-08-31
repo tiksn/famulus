f6ecb7a2495377f85d07d2bea333735617fd3c09
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-09-01 00:36:29 +0300

    show address

--- pkg/famulus/cmd/collect/collect.go
+++ pkg/famulus/cmd/collect/collect.go
@@ -12,11 +12,38 @@ func NewCollectCmd(c *config.Config) *cobra.Command {
 		Use:   "collect",
 		Short: "Collect Contacts",
 		Long:  "Collect Contacts",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Print("Collecting ....")
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return collectCmd(*c, args)
 		},
-		Args: cobra.ExactArgs(1),
+		Args: cobra.MaximumNArgs(1),
 	}
 
 	return cmd
+}
+
+func collectCmd(c config.Config, args []string) error {
+	argCount := len(args)
+
+	if argCount == 0 {
+		a, err := c.ListAddress()
+		if err != nil {
+			return err
+		}
+		for _, n := range a {
+			fmt.Println("Available collection option:", n)
+		}
+		return nil
+	} else if argCount == 1 {
+		adr, err2 := c.GetAddress(args[0])
+		if err2 != nil {
+			return err2
+		}
+		fmt.Println(adr)
+		// csv, err3 := c.GetContactsCsvFilePath()
+		// if err3 != nil {
+		// 	return err3
+		// }
+	}
+
+	return nil
 }
