d91580f1e7de30cc0c923322e0e58563fda1bc1b
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-09-08 11:57:00 +0300

    run tests

---
+++ test/number_parser_test.go
@@ -0,0 +1,17 @@
+package phone
+
+import (
+	"testing"
+
+	"github.com/tiksn/famulus/internal/pkg/phone"
+)
+
+func TestParse(t *testing.T) {
+	nums, err := phone.Parse("0689546321")
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	t.Log(nums)
+}
