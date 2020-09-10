1f9c6a82ebf86621f4e240f2fd4d1aa9723c8e7a
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-09-10 12:13:04 +0300

    scrap contacts

--- internal/pkg/scraper/scraper.go
+++ internal/pkg/scraper/scraper.go
@@ -1,8 +1,8 @@
 package scraper
 
 import (
+	"errors"
 	"fmt"
-	"log"
 	"net/http"
 	"regexp"
 	"strings"
@@ -10,23 +10,25 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func ListScrape(url string, phoneUrl string) {
+func ListScrape(url string, phoneUrl string) ([]Contact, error) {
 	fmt.Printf("Scraping list: %s", url)
 	fmt.Println()
 
 	res, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		return nil, fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
 	}
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
+
+	result := []Contact{}
 
 	doc.Find("div.main-content").ChildrenFiltered("div").Each(func(i int, s *goquery.Selection) {
 		s.Each(func(i int, mainDiv *goquery.Selection) {
@@ -35,29 +37,37 @@ func ListScrape(url string, phoneUrl string) {
 				element := element.First().Find("span a")
 				link, exists := element.Attr("href")
 				if exists {
-					ContactScrape(link, phoneUrl)
+					contact, err := ContactScrape(link, phoneUrl)
+					if err != nil {
+						fmt.Println(err)
+					} else {
+						result = append(result, contact)
+						fmt.Println(contact.GetNumbers())
+					}
 				}
 			}
 		})
 	})
+
+	return result, nil
 }
 
-func ContactScrape(url string, phoneUrl string) {
+func ContactScrape(url string, phoneUrl string) (Contact, error) {
 	fmt.Printf("Scraping contact: %s", url)
 	fmt.Println()
 
 	res, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		return nil, fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
 	}
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	element := doc.Find("body table:nth-child(8) tbody tr td:nth-child(1) table tbody tr")
 	if element != nil {
@@ -68,38 +78,50 @@ func ContactScrape(url string, phoneUrl string) {
 			re := regexp.MustCompile("^\\s*showPhonesWithDigits\\(\\'(?P<id>\\d*)\\',\\s*\\'(?P<key>[A-Fa-f0-9]{40})\\'\\)\\;\\s*return\\s*false\\s*\\;\\s*$")
 			matches := re.FindStringSubmatch(onclick)
 
-			NumbersScrape(matches[re.SubexpIndex("id")], matches[re.SubexpIndex("key")], phoneUrl)
+			nums, err := NumbersScrape(matches[re.SubexpIndex("id")], matches[re.SubexpIndex("key")], phoneUrl)
+			if err != nil {
+				return nil, err
+			}
+
+			return &contact{
+				numbers: nums,
+			}, nil
 		}
 	}
+
+	return nil, errors.New("Unable to find contact information")
 }
 
-func NumbersScrape(id string, key string, phoneUrl string) {
-	url := phoneUrl
-	method := "POST"
-
+func NumbersScrape(id string, key string, phoneUrl string) (string, error) {
 	payload := strings.NewReader(fmt.Sprintf("i=%s&s=%s", id, key))
 
 	client := &http.Client{}
-	req, err := http.NewRequest(method, url, payload)
+	req, err := http.NewRequest("POST", phoneUrl, payload)
 
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
 	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		return "", fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
 	}
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
+
 	element := doc.Find("div span")
 	if element != nil {
-		fmt.Println(element.Text())
+		return element.Text(), nil
+	} else {
+		return "", errors.New("Unable to find phone number")
 	}
 }
