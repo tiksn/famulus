10685bfadcd7a779b1cdaa0f218e8107186c2d34
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-12-15 12:32:37 +0200

    Update psakefile.ps1

--- scripts/psakefile.ps1
+++ scripts/psakefile.ps1
@@ -1,4 +1,28 @@
-Task UpdateVendors {
+Task Init {
+    $trashFolder = Join-Path -Path ".." -ChildPath ".trash"
+    $trashSubFolder = Get-Date -Format 'yyyyMMddHHmmss'
+    $script:trashFolder = Join-Path -Path $trashFolder -ChildPath $trashSubFolder
+    New-Item -Path $script:trashFolder -ItemType Directory | Out-Null
+    $script:trashFolder = Resolve-Path -Path $script:trashFolder
+    $script:rootFolder = Resolve-Path -Path ".." -Relative
+}
+
+Task Clean -Depends Init {
+    Exec { go clean } -workingDirectory $script:rootFolder
+}
+
+Task Format -Depends Clean {
+    Exec { go fmt .\cmd\famulus\ } -workingDirectory $script:rootFolder
+    Exec { go fmt .\internal\app\famulus\ } -workingDirectory $script:rootFolder
+    Exec { go fmt .\internal\pkg\people\ } -workingDirectory $script:rootFolder
+    Exec { go fmt .\internal\pkg\phone\ } -workingDirectory $script:rootFolder
+    Exec { go fmt .\internal\pkg\scraper\ } -workingDirectory $script:rootFolder
+    Exec { go fmt .\pkg\famulus\cmd\collect } -workingDirectory $script:rootFolder
+    Exec { go fmt .\pkg\famulus\cmd\root } -workingDirectory $script:rootFolder
+    Exec { go fmt .\test\ } -workingDirectory $script:rootFolder
+}
+
+Task UpdateVendors -depends Clean {
     Exec { go mod vendor }
 }
 
@@ -6,7 +30,7 @@ Task DownloadModules -depends UpdateVendors {
     Exec { go mod download }
 }
 
-Task TidyModules -depends DownloadModules {
+Task TidyModules -depends DownloadModules, Format {
     Exec { go mod tidy }
 }
 
